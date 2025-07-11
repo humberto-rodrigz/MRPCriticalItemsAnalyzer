7cd8fc4bd11b3328abcf15f5d506d6972d10c882
humberto-rodrigz <humberto7177@gmail.com>
2025-07-11 09:35:52 -0300

    add SQL integration

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import calendar
+import pandas as pd
+import ibm_db
+from datetime import datetime   
+import os
+import logging      
+import xlsxwriter
+from mrp_analyzer import connect_db, fetch_mrp_data, format_excel
+
+def salvar_excel_formatado(df, output_file):
+    """Salva o DataFrame em Excel formatado."""
+    writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name="Itens Críticos", index=False)
+    format_excel(writer, df)
+    writer.close()  
+
 
