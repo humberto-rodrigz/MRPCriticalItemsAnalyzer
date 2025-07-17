aa1a3eee970c87de6b64017dece983495202c5a1
humberto-rodrigz <humberto7177@gmail.com>
2025-07-17 09:05:26 -0300

    add automatic save

--- main.py
+++ main.py
@@ -23,4 +23,15 @@ def format_excel(writer, df):
     if not os.path.exists(directory):   
         os.makedirs(directory)  
 
+def salvar_excel_formatado(df, output_file):
+    """Salva o DataFrame em Excel formatado."""
+    writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name="Itens Críticos", index=False)
+    format_excel(writer, df)
+    writer.close()
+    """Formata o Excel com cores e bordas."""
+    workbook = writer.book  
+    worksheet = writer.sheets["Itens Críticos"]
+    cell_format = workbook.add_format({'border': 1, 'align': 'center', 'valign': 'vcenter'})
+    header_format = workbook.add_format({'bold': True, 'border': 1, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#D9EAD3'})
 
